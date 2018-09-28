b770185ecca30d96b3c34065e1fb2b9ba3fb6a60
AdibPr <pratama.adib@gmail.com>
2018-09-28 12:02:02 +0200

    Add imshow_tensor

--- utils.py
+++ utils.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 import sys
+import matplotlib.pyplot as plt
+import numpy as np
+
+import torchvision
 
 def progress_bar (*arg) :
     if len (arg) == 1 : 
@@ -17,3 +21,11 @@ def progress_bar (*arg) :
 
     sys.stdout.write('[%s] %s%s \r' % (bar, percents, '%'))
     sys.stdout.flush()
+
+def imshow_tensor(img):
+    if img.shape[0] != 3 : 
+        # means its a dataset, make grid first
+        img = torchvision.utils.make_grid (img)
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1,2,0)), interpolation='nearest')
+    plt.show ()
